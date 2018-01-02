ab0795f0b04e9387c099847a2fb1db6935e3639f
peigong <peigong.dh@qq.com>
2018-01-03 03:13:55 +0800

    move mud module to new repositories skynet-mud

--- preload/preload.lua
+++
@@ -1,7 +0,0 @@
--- This file will execute before every lua service start
-
-package.path = "./shared_lib/?.lua;" .. package.path
-package.path = "./shared_service/?.lua;" .. package.path
-package.path = "./config/?.lua;" .. package.path
-
-math.randomseed(os.time())
--- server/service/main.lua
+++ server/service/main.lua
@@ -1,6 +1,5 @@
 local skynet = require("skynet")
 local logger = require("logger")
-local sprotoloader = require("sprotoloader")
 
 skynet.start(function()
     logger.info("main", "Server start")
@@ -12,30 +11,6 @@ skynet.start(function()
 
     skynet.newservice("simpleweb")
     skynet.newservice("events")
-    -- skynet.newservice("testsocket", "", 0)
-
-    skynet.uniqueservice("protoloader")
-    skynet.uniqueservice("room_implement")
-    skynet.uniqueservice("persistent_implement")
-    skynet.uniqueservice("statelogging_implement")
-
-    local loginservice = skynet.uniqueservice("login_implement")
-    local gateservice = skynet.uniqueservice("gate_implement")
-    local watchdog = skynet.uniqueservice("watchdog")
-
-    local gateserver_host = skynet.getenv("gateserver_host")
-    local gateserver_port = tonumber(skynet.getenv("gateserver_port"))
-
-    local gateconf = {
-        address = gateserver_host,
-        port = gateserver_port,
-        nodelay = true,
-        maxclient = 1024,
-        servername = "gateserver1",
-        loginservice = loginservice,
-        gateservice = gateservice
-    }
-    skynet.call(watchdog, "lua", "start", gateconf)
 
     skynet.exit()
 end)
